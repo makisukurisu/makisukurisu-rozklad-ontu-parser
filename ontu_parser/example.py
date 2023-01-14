8f802ba22fb17f49204a52eaa7f87566692c0f91
Pavlo P <667strets@gmail.com>
2023-01-14 11:36:18 +0200

    Enhance example

--- ontu_parser/example.py
+++ ontu_parser/example.py
@@ -3,5 +3,13 @@ from classes import Parser
 
 parser = Parser()
 schedule = parser.parse(all_time=True)
-for day_name, pair in schedule.items():
-    print(f'{day_name} - {pair}')
+for day_name, pairs in schedule.items():
+    print(f'{day_name}:\n')
+    for pair in pairs:
+        if not pair.lessons:
+            continue
+        print(f'{pair.pair_no}:')
+        for lesson in pair.lessons:
+            print(f'{lesson.lesson_date}: {lesson.teacher["short"]} - {lesson.lesson_name["short"]}')
+            print(f'Card: {lesson.lesson_info}')
+            print()
